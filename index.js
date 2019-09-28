c0fb1fc50ec5af85182cf7e85149618bacaabc69
Oribuin <38044508+Oribuin@users.noreply.github.com>
2019-09-28 17:19:13 +0100

    Small Changes
    - Added a few Semi-Colons.
    - Added Comments to understand everything easier.
    - Changed the format of startup message.
    - Changed a bit of the sending msesage code
    - Removed Chalk (Useless)
    - Changed user joined Console.log Message.

--- index.js
+++ index.js
@@ -1,26 +1,41 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json');
-const chalk = require('chalk');
 
-client.once('ready', () => {
-	console.log(chalk.yellow('-------------------------------\n| Backyard MC Applications Bot |\n| Up and Running!              |\n| Version 0.1                  |\n| Made by: Spacella            |\n-------------------------------\n'));
+// Bot Startup Message
+client.on('ready', () => {
+    console.log('-------------------------------');
+    console.log("| Backyard MC Applications Bot |");
+    console.log("| Up and Running! |");
+    console.log("| Version 0.1 |");
+    console.log("| Made by: Spacella|");
+    console.log("-------------------------------");
 });
-
-client.on("guildMemberAdd" , member => {
+//When a member joins a guild (Guild isnt specified, may cause issues)
+client.on("guildMemberAdd", member => {
     var server = member.guild;
 
-    server.createChannel(member.displayName, "text")
-    const channel = client.channels.find("channelName", member.displayName)
-          const base = new Discord.RichEmbed()
-          .setTitle("Welcome to the BackyardMC Applications discord!")
-          .setColor(11044352)
-          .setDescription("Glad to know that you want to apply! React below for the position you would like to apply for. \n\n Thanks, BackyardMC Team")
-          .setImage("http://i.imgur.com/ysVpymuV.png")
-          .setTimestamp()
-      channel.send({base})
-      .then(console.log(chalk.magenta(member.displayName + ' application created.')))
-      .catch(console.error);
-});
+    //Create a channel with members name.
+    server.createChannel(member.displayName, "text");
+    // Find the channel name
+    const channel = client.channels.find("channelName", member.displayName);
 
+    // Embed Defining
+          const base = new Discord.RichEmbed()
+          base.setTitle("Welcome to the BackyardMC Applications discord!")
+          base.setColor(11044352) //What is this hex code?
+          base.setDescription("Glad to know that you want to apply! React below for the position you would like to apply for. \n\n Thanks, BackyardMC Team");
+          base.setImage("http://i.imgur.com/ysVpymuV.png");
+          base.setTimestamp();
+
+          // Send the embed to the channel.
+      channel.send(base).then(
+
+          console.log(`${member.user.tag} Created an application. | ${member.joinedAt.getTime} | `)
+          // Extra .thens here.
+
+          // Find any console errors (not sure if this will work.)
+      ).catch(console.error(console.log()));
+});
+// Connect the bot to discord through the bot token.
 client.login(config.token);
