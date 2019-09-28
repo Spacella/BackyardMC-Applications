d6c9063c83f9e9f5bde8615b16614abacc0bdefb
Ella <hello@spacella.xyz>
2019-09-28 12:22:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,23 +4,43 @@ const config = require('./config.json');
 const chalk = require('chalk');
 
 client.once('ready', () => {
-	console.log(chalk.yellow('-------------------------------\n| Backyard MC Applications Bot |\n| Up and Running!              |\n| Version 0.1                  |\n| Made by: Spacella            |\n-------------------------------\n'));
+	console.log(chalk.yellow('-------------------------------'));
+	console.log(chalk.yellow('| Backyard MC Applications Bot |'));
+	console.log(chalk.yellow('| Up and Running!              |'));
+	console.log(chalk.yellow('| Version 0.1                  |'));
+	console.log(chalk.yellow('| Made by: Spacella            |'));
+	console.log(chalk.yellow('-------------------------------'));
+	client.user.setActivity('for new applicants.', { type: 'WATCHING' });
 });
 
-client.on("guildMemberAdd" , member => {
-    var server = member.guild;
 
-    server.createChannel(member.displayName, "text")
-    const channel = client.channels.find("channelName", member.displayName)
-          const base = new Discord.RichEmbed()
-          .setTitle("Welcome to the BackyardMC Applications discord!")
-          .setColor(11044352)
-          .setDescription("Glad to know that you want to apply! React below for the position you would like to apply for. \n\n Thanks, BackyardMC Team")
-          .setImage("http://i.imgur.com/ysVpymuV.png")
-          .setTimestamp()
-      channel.send({base})
-      .then(console.log(chalk.magenta(member.displayName + ' application created.')))
-      .catch(console.error);
-});
+
+    client.on("guildMemberAdd" , member => {
+        var server = member.guild;
+
+        const base = new Discord.RichEmbed()
+        .setTitle("Welcome to the BackyardMC Applications discord!")
+        .setColor(11044352)
+        .setDescription("Glad to know that you want to apply! React below for the position you would like to apply for. \n\n Thanks, BackyardMC Team")
+        .addField('Moderation Staff - v')
+        .addField('Build Team - 🔨')
+        .addField('Graphic Designer - 🖥️')
+        .setTimestamp()
+          server.createChannel(member.displayName, "").then(channel => {
+              channel.overwritePermissions(server.id, {
+                  VIEW_CHANNEL: false
+              })
+              channel.overwritePermissions(member.id, {
+                 VIEW_CHANNEL: true
+              })
+              channel.send(base)
+                    .then(console.log(chalk.magenta(member.displayName + ' application created.')))
+                    .catch(console.error);
+              message.react('627533909097447424');
+              message.react('🔨');
+              message.react('🖥️');
+          })
+
+    });
 
 client.login(config.token);
