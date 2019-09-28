0d70586e4d557639f2166e1c77032427b20e6406
Oribuin <38044508+Oribuin@users.noreply.github.com>
2019-09-28 18:24:41 +0100

    New Changes
    Changes:
    - Removed Chalk. (Again)
    - Added comments for better understanding.
    - Added some semi colons. 
    - Replaced ' ' to " ".
    - Updated format and added more informational Console Log.
    - Added working Message Reactions.
    - Removed addField.
    addField originally had only one input, It would return null without a second input.

--- index.js
+++ index.js
@@ -1,46 +1,66 @@
-const Discord = require('discord.js');
+// Node.JS Packages
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const config = require('./config.json');
-const chalk = require('chalk');
+const config = require("./config.json");
 
-client.once('ready', () => {
-	console.log(chalk.yellow('-------------------------------'));
-	console.log(chalk.yellow('| Backyard MC Applications Bot |'));
-	console.log(chalk.yellow('| Up and Running!              |'));
-	console.log(chalk.yellow('| Version 0.1                  |'));
-	console.log(chalk.yellow('| Made by: Spacella            |'));
-	console.log(chalk.yellow('-------------------------------'));
-	client.user.setActivity('for new applicants.', { type: 'WATCHING' });
+// Don't use ' ', try using " " instead unless you're going to use ${} then use ` `
+
+// Bot Startup Message
+client.once("ready", () => {
+    console.log("-------------------------------");
+    console.log("| Backyard MC Applications Bot |");
+    console.log("| Up and Running!              |");
+    console.log("| Version 0.1                  |");
+    console.log("| Made by: Spacella            |");
+    console.log("-------------------------------");
+
+    //Set the activity of the bot
+    client.user.setActivity("for new applicants.", { type: "WATCHING" });
 });
 
+/*
+When a Member Joins the Guild it runs the following:
+Guild is undefined, having the bot in multiple servers may cause issues.
+*/
 
+client.on("guildMemberAdd", member => {
+    // Server Variable
+    var server = member.guild;
 
-    client.on("guildMemberAdd" , member => {
-        var server = member.guild;
+    // Discord Embed defined.
 
-        const base = new Discord.RichEmbed()
-        .setTitle("Welcome to the BackyardMC Applications discord!")
-        .setColor(11044352)
-        .setDescription("Glad to know that you want to apply! React below for the position you would like to apply for. \n\n Thanks, BackyardMC Team")
-        .addField('Moderation Staff - v')
-        .addField('Build Team - 🔨')
-        .addField('Graphic Designer - 🖥️')
-        .setTimestamp()
-          server.createChannel(member.displayName, "").then(channel => {
-              channel.overwritePermissions(server.id, {
-                  VIEW_CHANNEL: false
-              })
-              channel.overwritePermissions(member.id, {
-                 VIEW_CHANNEL: true
-              })
-              channel.send(base)
-                    .then(console.log(chalk.magenta(member.displayName + ' application created.')))
-                    .catch(console.error);
-              message.react('627533909097447424');
-              message.react('🔨');
-              message.react('🖥️');
-          })
+    const base = new Discord.RichEmbed();
+	// Embed Contents
+    base.setTitle("Welcome to the BackyardMC Applications Discord!");
+    base.setColor(11044352); // What color is this?
+    // Description of the Embed
+    base.setDescription("Glad to know that you want to apply! React below for the position you would like to apply for. \n\n" +
+        "Thanks, BackyardMC Team \n \n" +
+        "Moderation Staff - <emoji>\n" +
+        "Build Team - :hammer:\n" +
+        "Graphic Designer - <emoji>");
 
-    });
+    // Create a channel with the members name
+    server.createChannel(member.displayName, "").then(channel => {
 
-client.login(config.token);
+        //Set the Channel Permisions
+        channel.overwritePermissions(server.id, {
+            VIEW_CHANNEL: false
+        });
+
+        channel.overwritePermissions(member.id, {
+            VIEW_CHANNEL: true
+        });
+
+        // Send the embed then react.
+        channel.send(base).then(x => x.react("627533909097447424").then(x.react("🔨")).then(x.react("🖥️")));
+
+        // Log the Creation of Application
+        console.log(chalk.magenta(`${member.displayName} Created joined and Application.\n \n`,
+            `Time: ${member.joinedTimestamp}\n`,
+            `Day: ${member.joinedAt.getDay}\n`,
+            `Month: ${member.joinedAt.getMonth}`));
+    })
+});
+
+client.login(`${config.token}`);
